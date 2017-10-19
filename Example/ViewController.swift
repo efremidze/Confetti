6c14a645647e773759f8782c2045b0474c0a5b29
Lasha Efremidze <efremidzel@hotmail.com>
2017-10-19 00:39:25 -0700

    Added Confetti

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Confetti
 
 class ViewController: UIViewController {
 
---
+++ Sources/Confetti.swift
@@ -0,0 +1,11 @@
+//
+//  Confetti.swift
+//  Confetti
+//
+//  Created by Lasha Efremidze on 10/19/17.
+//  Copyright © 2017 Lasha Efremidze. All rights reserved.
+//
+
+import Foundation
+
+struct Confetti {}
